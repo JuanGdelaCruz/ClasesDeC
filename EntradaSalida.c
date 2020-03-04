46bed5254c8639fce5ad4c04fdd1060aa23af28b
JuanGdelaCruz <juangarciacruz.developer@gmail.com>
2020-03-04 21:53:41 +0100

    no message

--- EntradaSalida.c
+++ EntradaSalida.c
@@ -4,11 +4,6 @@
 
 int main(int argc, char *argv[])
 {
-  //Aquí simplemento pongo el bufer de entrada y salida a 0 para que siempre saque todo por pantalla.
-  //No hace falta mirarlo.
-  setbuf(stdout, NULL);
-  setbuf(stdin, NULL);
-
   char buffer[512];
   int leido = 0;
   while ((leido = read(0, &buffer, 256)) > 0)
