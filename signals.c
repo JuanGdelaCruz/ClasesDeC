2fb993476198b58c49d4182ce059eed89a02940a
JuanGdelaCruz <juangarciacruz.developer@gmail.com>
2020-03-17 01:47:06 +0100

    Código comentado.

--- signals.c
+++ signals.c
@@ -8,9 +8,7 @@ void my_handler();
 
 int main(int argc, char *argv[])
 {
-    setbuf(stdout, NULL); /* Unbuffered */
-    setbuf(stdin, NULL);
-
+ 
     printf("This program intends to provide an insight of how signals can be used, and therefore handled.\n");
     char input[64];
     int ignore = -1, handle = -1;
@@ -22,9 +20,13 @@ int main(int argc, char *argv[])
         handle = strcmp(input, "H");
     }
 
+    //Declaramos un struct del tipo sigaction.
     struct sigaction act;
+    //This flag controls what happens when a signal is delivered during certain primitives (such as open, read or write).
     act.sa_flags = SA_RESTART;
+    //Si el usuario ha dicho que la señal ha de ser ignorada, se ignora, y si ha de ser manejada se maneja con la función definida más abajo (my_handler).
     act.sa_handler = (ignore == 0) ? SIG_IGN : my_handler;
+    // Asignamos el tipo de respuesta que hemos definido con el struct a la señal 'SIGINT' (interrupción).
     sigaction(SIGINT, &act, NULL);
 
     printf("A dumb prompt will appear now, type \'exit\' to end the process.\n");
